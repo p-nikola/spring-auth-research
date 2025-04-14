414dac10fc44b6168f778e7385466cad508eb1fd
p-nikola <petrushevski.nikola@gmail.com>
2025-04-14 22:45:00 +0200

    initial commit

---
+++ src/main/java/com/example/springauthresearch/SpringAuthResearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.springauthresearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthResearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAuthResearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springauthresearch/SpringAuthResearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springauthresearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthResearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
